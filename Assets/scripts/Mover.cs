4185744fd8d907ae2e727ffd3af5bbefa30214f4
Kamrul-Haque <kamrul35-255@diu.edu.bd>
2021-11-13 22:14:19 +0600

    Backing Up

---
+++ Assets/scripts/Mover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(100, 500));   
+    }
+}
